5a2eda7e21b50e2bec2858e2037cb7a79f7d94f3
Ariel Malka <ariel@chronotext.org>
2014-01-19 17:03:35 +0200

    REFACTORING

--- android/Android.mk
+++ android/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := ICUCommon
-LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libICUCommon.a
+LOCAL_MODULE := icu-common
+LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libicu-common.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include/icu/common
 include $(PREBUILT_STATIC_LIBRARY)
 
-LOCAL_MODULE := HB
-LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libHB.a
+LOCAL_MODULE := hb
+LOCAL_SRC_FILES := ../lib/android/$(TARGET_ARCH_ABI)/libhb.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include/hb
 include $(PREBUILT_STATIC_LIBRARY)
